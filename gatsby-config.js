0cacc1dc4a27f449c1d6898267d0445e009bf61a
Ethan Bellora <Belloramail@gmail.com>
2018-10-03 10:40:23 -0400

    added plugin to help load fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,15 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Mr De Haviland`,
+          `Open Sans\:400,700` // you can also specify font weights and styles
+        ]
+      }
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: 'Zoe B Homepage',
